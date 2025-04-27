5211b4805ec208042a41e61ef409a893e71f72bb
Huitaek Yun <htyun@kaist.ac.kr>
2025-04-28 08:29:29 +0900

    Third commit

--- py/check_attendance.py
+++ py/check_attendance.py
@@ -4,7 +4,20 @@ print(os.getcwd())
 with open('py/student_data.json', 'r',) as file:
     data = json.load(file)
 
+''' --------------------------------------------'''
+def htyun():
+    print("I love programming.")
+
+
+# Add your introduction
+''' --------------------------------------------'''
+
 for item in data:
     print(f"Name: {item['name']}, Major: {item['major']}, Role: {item['role']}")
+    ''' --------------------------------------------'''
     if item['name'] == 'Huitaek Yun':
-        print("I love programming.")
+        htyun()   
+    
+    ## Add your own function call
+
+    ''' --------------------------------------------'''
