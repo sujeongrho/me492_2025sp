acc8f13c43fade18071f04ce443c5cf868cd5d55
Huitaek Yun <htyun@kaist.ac.kr>
2025-04-28 08:32:33 +0900

    This is where our collaboration starts

--- py/check_attendance.py
+++ py/check_attendance.py
@@ -5,19 +5,19 @@ with open('py/student_data.json', 'r',) as file:
     data = json.load(file)
 
 ''' --------------------------------------------'''
+# Add your introduction
 def htyun():
     print("I love programming.")
-
-
-# Add your introduction
+def hanbin_jang():
+    print("I am the grader of this homework")
 ''' --------------------------------------------'''
 
 for item in data:
     print(f"Name: {item['name']}, Major: {item['major']}, Role: {item['role']}")
     ''' --------------------------------------------'''
+    ## Add your own function call
     if item['name'] == 'Huitaek Yun':
         htyun()   
-    
-    ## Add your own function call
-
+    if item['name'] == 'Hanbin Jang':
+        hanbin_jang()  
     ''' --------------------------------------------'''
